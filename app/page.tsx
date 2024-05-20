fdd2bafebb64a7c4febe28dad1884180cd5b6191
Yhawbee1 <antwibrian82@gmail.com>
2024-05-20 19:30:58 +0000

    Create Testimonial section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { FaHome } from "react-icons/fa";
 import RecentProjects from "../components/RecentProject";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience  />
       </div>
     </main>
   );
--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { companies, testimonials } from "@/data";
 
 const Clients = () => {
   return (
-    <div>Clients</div>
-  )
-}
+    <div className="py-20" id="testimonials">
+      <h1 className="heading">
+        Kind Words from {" "}
+        <span className="text-purple">satisfied Clients</span>
+      </h1>
+          <div className="flex flex-col items-center max-lg:mt-10">
+              
+                  <InfiniteMovingCards
+                  items={testimonials}
+                  direction="right"
+                  speed="slow"
+              />
+              <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg: mt-10">
+                  {companies.map(({ id, img, name, nameImg }) => (
+                      <div className="flex md:max-w-60 max-w-32 gap-2" key={id}>
+                          <img src={img} alt={name} className="md:w-10 w-5" />
+                          <img src={nameImg} alt={name} className="md:w-24 w-20" />
+                      </div>
+                  ))}
+            </div>
+              
+      </div>
+    </div>
+  );
+};
 
-export default Clients
+export default Clients;
